ed1374f479bd73cf04ab55ebe70ec0510ae3da2b
vikivoskoboinykarma <114704416+vikivoskoboinykarma@users.noreply.github.com>
2023-11-21 00:10:25 -0700

    update2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 const galleryList = document.querySelector('.gallery');
 
-function createGalleryItem({ preview, original, description }) {
-  const listItem = document.createElement('li');
-  listItem.classList.add('gallery__item');
-
-  const link = document.createElement('a');
-  link.classList.add('gallery__link');
-  link.href = original;
-
-  const image = document.createElement('img');
-  image.classList.add('gallery__image');
-  image.src = preview;
-  image.alt = description;
-  link.appendChild(image);
-  listItem.appendChild(link);
-
-  return listItem;
+function createMarkup(arr) {
+  return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+        </a>
+    </li>`
+  ).join("");
 }
 
 function renderGallery() {
-  const galleryItemsMarkup = galleryItems.map(createGalleryItem);
-  galleryList.append(...galleryItemsMarkup);
-}
-
-renderGallery();
-
-//  SimpleLightbox
-const lightbox = new SimpleLightbox('.gallery a', {
+    const galleryItemsMarkup = createMarkup(galleryItems);
+    galleryList.innerHTML = galleryItemsMarkup;
+  }
+  
+  renderGallery();
+  
+  // SimpleLightbox
+  const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
   });
-  
